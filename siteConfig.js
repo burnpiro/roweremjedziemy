072f324e04a48033b3d194519abd40fcbcafad28
burnpiro <kemalpiro@gmail.com>
2020-08-20 21:23:16 +0200

    Initial commit

---
+++ siteConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: 'Rowerem Jedziemy | blog',
+  shortName: '',
+  description: 'blog o podróżowaniu i nie tylko',
+  url: 'https://roweremjedziemy.pl', // Domain of your site without prefix!
+  prefix: '/',
+  author: 'Kemal Erdem',
+  twitter: 'burnpiro',
+  fb: 'pirokemal',
+  github: 'burnpiro'
+}
