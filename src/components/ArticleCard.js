6865db5a4823f5192a1ebc4c3c0152bdc544a8df
burnpiro <kemalpiro@gmail.com>
2020-08-27 21:24:48 +0200

    Create similar article card component to display articles

---
+++ src/components/ArticleCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Img from 'gatsby-image';
+
+export default ({ slug, thumbnail, title }) => {
+  console.log(slug)
+  return (
+    <div className="similar-card">
+      <Link to={slug} className="similar-card-link">
+        {
+          thumbnail && <Img
+            fluid={thumbnail.childImageSharp.fluid}
+            className="similar-card-image"
+          />
+        }
+        <div className="similar-card-content">
+          <h2 className="similar-card-title">{title}</h2>
+        </div>
+      </Link>
+    </div>
+  );
+};
